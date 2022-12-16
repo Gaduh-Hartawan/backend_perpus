69ec52d953a6f54975382aa64f3a16a67ebd0c68
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-12-16 09:00:13 +0700

    menambah route pinjam dan history

--- routes/user.js
+++ routes/user.js
@@ -2,3 +2,8 @@ import express from "express";
 import { listBorrow, historyUser } from "../controllers/user.js";
 
 const router = express.Router();
+
+router.get("/listborrow", listBorrow);
+router.get("/history", historyUser);
+
+export default router;
