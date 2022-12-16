a22f1455b25a1b102983e4062358be9ea944efc9
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-12-16 08:59:42 +0700

    inisialisasi route user

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import AdminROute from "./routes/admin.js";
 import cors from "cors";
+import AdminRoute from "./routes/admin.js";
+import UserRoute from "./routes/user.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(AdminROute);
+app.use(AdminRoute);
+app.use(UserRoute);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
