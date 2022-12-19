ffe589f23c8584797b1d5c677e1fdb3df39b1a9c
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-12-19 07:10:25 +0700

    inisialisasi bookroute

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import AdminRoute from "./routes/admin.js";
 import UserRoute from "./routes/user.js";
+import BookRoute from "./routes/books.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(AdminRoute);
 app.use(UserRoute);
+app.use(BookRoute);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
