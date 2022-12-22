5f22b890755e24370c94b871d17c745f6af0e9c7
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-12-22 13:52:31 +0700

    tambah parameter id di route edit

--- routes/books.js
+++ routes/books.js
@@ -1,27 +1,28 @@
 import express from "express";
-import { getBooks,
-         addBook,
-         getBookById,
-         editBook,
-         deleteBook,
-         getKategori,
-         addKategori,
-         getKategoriById,
-         editKategori, 
-         deleteKategori, 
-         getJenis,
-         addJenis,
-         getJenisById,
-         editJenis,
-         deleteJenis,
-}from "../controllers/books.js";
+import {
+  getBooks,
+  addBook,
+  getBookById,
+  editBook,
+  deleteBook,
+  getKategori,
+  addKategori,
+  getKategoriById,
+  editKategori,
+  deleteKategori,
+  getJenis,
+  addJenis,
+  getJenisById,
+  editJenis,
+  deleteJenis,
+} from "../controllers/books.js";
 
 const router = express.Router();
 
 router.get("/books", getBooks);
 router.post("/books", addBook);
 router.get("/books/:id", getBookById);
-router.put("/books", editBook);
+router.put("/books/:id", editBook);
 router.delete("/books/:id", deleteBook);
 
 router.get("/category", getKategori);
